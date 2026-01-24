101287664f2fffda5444b7b0f2929907e5feb395
Broady <broadybmx@gmail.com>
2026-01-24 11:09:41 -0500

    Updated modal

--- app/components/modal.tsx
+++ app/components/modal.tsx
@@ -31,9 +31,15 @@ export default function Modal() {
   return (
     <div className="modal text-white rounded-md border border-green-500/40 rounded-lgfont-mono">
       <div className="modal__wrapper p-[24px] mx-auto">
-        <div className="modal__title text-center text-green-400 text-[48px] pb-[12px]">_______________</div>
-        <div className="modal__sub-title text-center text-green-400 text-[32px] pb-[12px]">_______________</div>
-        <div className="modal__sub-title-2 text-center text-green-400 text-[32px]">_______________</div>
+        <div className="modal__title text-center text-green-400 text-[32px] lg:text-[48px] pb=[4px] lg:pb-[12px]">
+          _______________
+        </div>
+        <div className="modal__sub-title text-center text-green-400 text-[24px] lg:text-[32px] pb=[4px] lg:pb-[12px]">
+          _______________
+        </div>
+        <div className="modal__sub-title-2 text-center text-green-400 text-[24px] lg:text-[32px]">
+          _______________
+        </div>
         <form className="mt-6 space-y-4">
           <div className="flex flex-col gap-2">
             <label htmlFor="user_name" className="text-green-400 text-sm">
@@ -73,9 +79,14 @@ export default function Modal() {
               className="rounded-md border border-green-500/40 bg-transparent p-2 text-white focus:outline-none focus:border-green-400"
             />
           </div>
-          <button id="contact__submit" className="form__submit">
-            Send Message
-          </button>
+          <div className="button__wrapper flex justify-center w-full">
+            <button
+              id="contact__submit"
+              className="form__submit w-full max-w-[240px] text-green-400 border border-green-500/40 text-[20px] py-[12px] px-[24px] hover:bg-green-500/40 hover:text-black"
+            >
+              Send Message
+            </button>
+          </div>
         </form>
       </div>
     </div>
